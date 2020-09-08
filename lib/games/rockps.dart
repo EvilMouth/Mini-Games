acb823754977f8d8208a27cc05386264ceb45720
EVIL MOUTH <izyhang@gmail.com>
2020-09-08 18:30:18 +0800

    rockps game

--- lib/games/rockps.dart
+++ lib/games/rockps.dart
@@ -1,11 +1,62 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RockPaperScissorsGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('RPS Game')),
+    return ChangeNotifierProvider(
+      create: (context) => _ViewModel(),
+      child: Builder(
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text('RPS Game')),
+          body: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('游戏规则：1s内输给我'),
+              ),
+              Center(
+                child: Consumer<_ViewModel>(
+                  builder: (context, viewModel, child) => IndexedStack(
+                    index: viewModel.index,
+                    children: _ViewModel.assets
+                        .map<Widget>((e) => Image.asset(e))
+                        .toList(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              context.read<_ViewModel>().refresh();
+            },
+            child: Icon(Icons.refresh),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+class _ViewModel with ChangeNotifier {
+  static const assets = [
+    'images/rock.png',
+    'images/paper.png',
+    'images/scissors.png',
+  ];
+  int _index = 0;
+  int get index => _index;
+
+  _ViewModel() {
+    refresh();
+  }
+
+  refresh() {
+    _index = Random().nextInt(assets.length);
+    notifyListeners();
   }
 }
